52756c3a60a6dbbddf1fca98381bd33e9e27d714
UshaUthirasamy <80030587+UshaUthirasamy@users.noreply.github.com>
2021-12-11 15:34:15 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Print from '/home/usha/Desktop/OddNumber/oddnumber/src/print.js'
+
+function App() {
+  return (
+    <Print />
+  );
+}
+
+export default App;
---
+++ print.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react'
+import './App.css';
+
+class Print extends Component {
+
+    render() {
+        var value = 0;
+        var array = [1, 2, 3];
+        var even = [];
+        var odd = [];
+        var evens = false;
+        var odds = false;
+
+        function getData(val) {
+            value = val.target.value;
+        }
+
+        function handle() {
+            for (let i = 0; i < value; i++) {
+                array[i] = Math.floor(Math.random() * 100) + 1;
+            }
+            console.log(array);
+        }
+
+        function oddDisplay() {
+            if (evens === false) {
+            odds = true;
+            }
+            else {
+                evens = false;
+                odds = true;
+            }
+            let i = 0;
+            odd = [];
+            console.log(array.length);
+            while (i <= array.length) {
+                if (array[i] % 2 !== 0) {
+                    if (array[i] !== undefined) {
+                        odd.push(array[i]);
+                    }
+                }
+                i++;
+            }
+            console.log(odd);
+        }
+
+        function evenDisplay() {
+            if (odds === false) {
+            evens = true;
+            } else {
+                odds = false;
+                evens = true;
+            }
+            let i = 0;
+            even = [];
+            console.log(array.length);
+            while (i <= array.length) {
+                if (array[i] % 2 === 0) {
+                    even.push(array[i]);
+                }
+                i++;
+            }
+            console.log(even);
+        }
+        return (
+            <>
+                <header>
+                    <div className="heading">NUMBER FILTER</div>
+                </header>
+                <br></br>
+                <div>
+                    <input type="text" placeholder="Enter the number of inputs" className="numbers" onChange={getData}></input>
+                </div><br></br>
+                <div>
+                    <button className="random" onClick={handle}>Generate Random Numbers</button>
+                </div><br></br>
+                <div className="buttons">
+                    <button onClick={oddDisplay}>ODD</button>&emsp;<span><button onClick={evenDisplay}>EVEN</button></span>
+                </div>
+                <div>
+                    <h2 className="heading">Filtered Numbers</h2>
+                    {odds ? <h1>{odd.join(", ")}</h1> : <h1>{even.join(", ")}</h1> }
+                </div>
+            </>
+        )
+    }
+}
+
+export default Print;
